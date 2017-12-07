0c720db80806f0f691d2528a6a20f25ff5210b1b
Erzhan <kk.erzhan@gmail.com>
2017-12-07 09:52:05 +0600

    Add files via upload

--- sketch_nov14a.ino
+++ sketch_nov14a.ino
@@ -1,7 +1,7 @@
 void setup() {
   Serial.begin(9600);
   Serial1.begin(9600);
-  Serial2.begin(9600);
+  //Serial2.begin(9600);
 }
 
 int stoif(String s) {
@@ -25,26 +25,54 @@ int stoif(String s) {
   return n;
 }
 
+struct stick {
+  int x;
+  int y;
+};
 struct driver {
-  int lmSpeed = 0;
-  int rmSpeed = 0;
-  bool lb1 = false;
-  bool lb2 = false;
-  bool rb2 = false;
-  bool rb1 = false;
+  stick leftStick;
+  stick rightStick;
 
+  bool buttonY;
+  bool buttonB;
+  bool buttonA;
+  bool buttonX;
+
+  bool buttonLB;
+  bool buttonRB;
+  bool buttonLT;
+  bool buttonRT;
+
+  bool dpadUp;
+  bool dpadRight;
+  bool dpadDown;
+  bool dpadLeft;
+
+  bool precisionEnabled;
   void parseCommand(String c) {
+    //Serial.println(c);
     String cmd = "";
     int num = 0;
     for(int i = 0; i < c.length(); i++) {
       if(c[i] == ';') {
         switch(num) {
-          case 0: lmSpeed = stoif(cmd); break;
-          case 1: rmSpeed = stoif(cmd); break;
-          case 2: lb1 = (cmd == "1"); break;
-          case 3: lb2 = (cmd == "1"); break;
-          case 4: rb2 = (cmd == "1"); break;
-          case 5: rb1 = (cmd == "1"); break;
+          case 0: leftStick.x = stoif(cmd); break;
+          case 1: leftStick.y = stoif(cmd);  break;
+          case 2: rightStick.x = stoif(cmd); break;
+          case 3: rightStick.y = stoif(cmd); break;
+          case 4: buttonY = (cmd == "1"); break;
+          case 5: buttonB = (cmd == "1"); break;
+          case 6: buttonA = (cmd == "1"); break;
+          case 7: buttonX = (cmd == "1"); break;
+          case 8: buttonLB = (cmd == "1"); break;
+          case 9: buttonLT = (cmd == "1"); break;
+          case 10: buttonRB = (cmd == "1"); break;
+          case 11: buttonRT = (cmd == "1"); break;
+          case 12: dpadUp = (cmd == "1"); break;
+          case 13: dpadRight = (cmd == "1"); break;
+          case 14: dpadDown = (cmd == "1"); break;
+          case 15: dpadLeft = (cmd == "1"); break;
+          case 16: precisionEnabled = (cmd == "1"); break;
        }
        cmd = "";
        num++;
@@ -68,37 +96,10 @@ int convertBtToAnalog(int in) {
 }
 
 //PINS
-int wheelL_1 = 22;
-int wheelL_2 = 23;
-int wheelL_S = 2;
 
-int wheelR_1 = 24;
-int wheelR_2 = 25;
-int wheelR_S = 3;
 
-int lift_1 = 26;
-int lift_2 = 27;
-
-int claw_1 = 28;
-int claw_2 = 29;
-
-int projectileClaw_X_1 = 30;
-int projectileClaw_X_2 = 31;
-int projectileClaw_X_S = 4;
-
-int projectileClaw_Y_1 = 32;
-int projectileClaw_Y_2 = 33;
-int projectileClaw_Y_S = 5;
-
-int catapult_mover_1 = 34;
-int catapult_mover_2 = 35;
-
-int catapult_holder_1 = 36;
-int catapult_holder_2 = 37;
-int catapult_holder_speed = 6;
-
-driver d1, d2;
-String cmd_d1, cmd_d2;
+driver d1;
+String cmd_d1;
 
 void configureMotor(int pin1, int pin2, int pinS, int stick, int desiredSpeed = -1) {
   if(desiredSpeed == -1) {
@@ -107,6 +108,7 @@ void configureMotor(int pin1, int pin2, int pinS, int stick, int desiredSpeed = 
   else {
     analogWrite(pinS, desiredSpeed);
   }
+  //Serial.println(stick);
   if(stick > 0) {
     digitalWrite(pin1, HIGH);
     digitalWrite(pin2, LOW);
@@ -136,55 +138,96 @@ void configureMotorBoolean(int pin1, int pin2, bool btn1, bool btn2) {
   }
 }
 
-void configureCatapultHolder(bool btn1, bool btn2) {
-  configureMotorBoolean(catapult_holder_1, catapult_holder_2, btn1, btn2);
-  if(btn1) {
-    analogWrite(catapult_holder_speed, 255);
-  }
-  else if(btn2) {
-    analogWrite(catapult_holder_speed, 255);
-  }
-  else {
-    analogWrite(catapult_holder_speed, 0);
-  }
-}
+//Pins
+int rightWheels_first_1 = 24;
+int rightWheels_first_2 = 25;
+int rightWheels_first_s = 3;
+int rightWheels_second_1 = 22;
+int rightWheels_second_2 = 23;
+int rightWheels_second_s = 2;
 
+int leftWheels_first_1 = 34;
+int leftWheels_first_2 = 35;
+int leftWheels_first_s = 4;
+
+int leftWheels_second_1 = 36;
+int leftWheels_second_2 = 37;
+int leftWheels_second_s = 5;
+
+int liftFirst_1 = 26;
+int liftFirst_2 = 27;
+
+int clawFirst_1 = 28;
+int clawFirst_2 = 29;
+
+int liftSecond_1 = 30;
+int liftSecond_2 = 31;
+
+int clawSecond_1 = 32;
+int clawSecond_2 = 33;
+
+void debugSerial() {
+  Serial.print(d1.leftStick.x);
+  Serial.print(" ");
+  Serial.print(d1.leftStick.y);
+  Serial.print(" ");
+  Serial.print(d1.rightStick.x);
+  Serial.print(" ");
+  Serial.print(d1.rightStick.y);
+  Serial.print(" ");
+  Serial.print(d1.buttonY);
+  Serial.print(" ");
+  Serial.print(d1.buttonB);
+  Serial.print(" ");
+  Serial.print(d1.buttonA);
+  Serial.print(" ");
+  Serial.print(d1.buttonX);
+  Serial.print(" ");
+  Serial.print(d1.buttonLB);
+  Serial.print(" ");
+  Serial.print(d1.buttonLT);
+  Serial.print(" ");
+  Serial.print(d1.buttonRB);
+  Serial.print(" ");
+  Serial.print(d1.buttonRT);
+  Serial.print("\n");
+}
 void loop() {
-  if(Serial1.available()) {
+  //digitalWrite(2, HIGH);
+  //digitalWrite(3, LOW);
+  while(Serial1.available()) {
     char x = Serial1.read();
     //Serial.print(x);
     if(x != '#') {
       cmd_d1 += x;
     }
     else {  
-      Serial.println(cmd_d2);
+      //Serial.println(cmd_d1);
+      //qqSerial.println(cmd_d1);
       d1.parseCommand(cmd_d1);
       cmd_d1 = "";
     }
   }
-
-  if(Serial2.available()) {
-    char x = Serial2.read();
-    //Serial.print(x);
-    if(x != '#') {
-      cmd_d2 += x;
-    }
-    else {
-      d2.parseCommand(cmd_d2);
-      cmd_d2 = "";
-    }
+  //debugSerial();
+  //Serial.println(d1.leftStick.y);
+  double multiplier = 1.0;
+  if(d1.precisionEnabled) {
+    multiplier = 0.2;
   }
+  configureMotor(leftWheels_first_2, leftWheels_first_1, leftWheels_first_s, d1.leftStick.y * multiplier);
+  configureMotor(leftWheels_second_2, leftWheels_second_1, leftWheels_second_s, d1.leftStick.y * multiplier);
+  
+  configureMotor(rightWheels_first_1, rightWheels_first_2, rightWheels_first_s, d1.rightStick.y);
+  configureMotor(rightWheels_second_1, rightWheels_second_2, rightWheels_second_s, d1.rightStick.y);
 
-  //Serial.println(lb1);
-  configureMotor(wheelL_1, wheelL_2, wheelL_S, d1.lmSpeed);
-  configureMotor(wheelR_1, wheelR_2, wheelR_S, d1.rmSpeed);
-  configureMotorBoolean(lift_1, lift_2, d1.lb1, d1.lb2);
-  configureMotorBoolean(claw_1, claw_2, d1.rb1, d1.rb2);
+  //Serial.println(d1.selectedClaw);
+  configureMotorBoolean(liftFirst_1, liftFirst_2, d1.buttonLB, d1.buttonLT);
+  configureMotorBoolean(liftSecond_1, liftSecond_2, d1.buttonRT, d1.buttonRB);
 
-  configureMotor(projectileClaw_X_1, projectileClaw_X_2, projectileClaw_X_S, d2.lmSpeed);
-  configureMotor(projectileClaw_Y_1, projectileClaw_Y_2, projectileClaw_Y_S, d2.rmSpeed);
+  configureMotorBoolean(clawFirst_1, clawFirst_2, d1.buttonY, d1.buttonX);
+  configureMotorBoolean(clawSecond_1, clawSecond_2, d1.buttonA, d1.buttonB);
 
-  configureMotorBoolean(catapult_mover_1, catapult_mover_2, d2.lb1, d2.lb2);
-  configureCatapultHolder(d2.rb1, d2.rb2);
+  // TODO : ADD THIRD CLAW
+  delay(10);
 }
 
